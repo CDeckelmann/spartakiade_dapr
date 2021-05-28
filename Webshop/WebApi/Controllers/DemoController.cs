ed037a76d8c296740562b9e7a50de30aa87f39c9
Koszior, Daniel <Daniel.Koszior@comparex.com>
2021-05-28 15:39:17 +0200

    Web Api added

---
+++ Webshop/WebApi/Controllers/DemoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Client;
+
+namespace WebApi.Controllers
+{
+    public class Body
+    {
+        public string Message { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private const string StoreName = "statestore";
+        private const string StoreKey = "statestore";
+
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IList<Tuple<string, DateTime>>> GetMessage([FromServices] DaprClient daprClient)
+        {
+            var state = await daprClient.GetStateEntryAsync<IList<Tuple<string, DateTime>>>(StoreName, StoreKey);
+            return state.Value;
+        }
+
+        [HttpPost]
+        public async Task SetMessage([FromBody] Body message, [FromServices] DaprClient daprClient)
+        {
+            var state = await daprClient.GetStateEntryAsync<IList<Tuple<string, DateTime>>>(StoreName, StoreKey);
+            if (state.Value == null)
+            {
+                state.Value = new List<Tuple<string, DateTime>>{ Tuple.Create(message.Message, DateTime.UtcNow) };
+            }
+            else
+            {
+                state.Value.Add(Tuple.Create(message.Message, DateTime.UtcNow));
+            }
+
+            await state.SaveAsync();
+        }
+    }
+}
